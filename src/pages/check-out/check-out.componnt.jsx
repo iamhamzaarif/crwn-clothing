d378ef6f0e969a39e796e8463664422cef2fe8c5
Iamhamzaarif <hamza.arif5587@gmail.com>
2020-04-15 02:37:35 +0500

    updating our stripe integration and updating our fav icon and title

--- src/pages/check-out/check-out.componnt.jsx
+++ src/pages/check-out/check-out.componnt.jsx
@@ -28,16 +28,14 @@ const CheckoutPage = ({cartItems,total}) => (
         </div>
         {cartItems.map(cartItem => <CheckoutItems key={cartItem.id} cartItem={cartItem}/>)}
 
-    <div className='total'>
-            <span>TOTAL: ${total}</span>
+        <div className='total'>TOTAL: ${total}</div>
     <div className='test-warning'>
-        *Please use following test credit card for payment*
-        <br/>
-        4242 4242 4242 4242 - Exp: 10/20 - CVV: 123
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
     </div>
-    </div>
-    <StripeCheckoutButton price={total}/>
-    </div>
+    <StripeCheckoutButton price={total} />
+  </div>
 
 )
 
